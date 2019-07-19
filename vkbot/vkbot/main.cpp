402e8300e5a57b64ec87a08a6dcbc8084f4379dd
andermirik <33791601+andermirik@users.noreply.github.com>
2019-07-19 11:00:43 +0300

    add json

--- vkbot/vkbot/main.cpp
+++ vkbot/vkbot/main.cpp
@@ -1,9 +1,18 @@
 #include "http/http.h"
+#include <fstream>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 int main() {
+	//184605473
+
+	std::string token;
+	std::ifstream fin("config/apikey.txt");
+	fin >> token;
 
 	while (true) {
-
+		
+		auto a = http::post("https://api.vk.com/method/groups.getLongPollServer?access_token="+token+"&v=5.101&group_id=184605473");
 	}
 
 	return(0);
