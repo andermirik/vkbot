155fbf6ebf9f795d9a0b63c023b802c73ef8fc04
andermirik <33791601+andermirik@users.noreply.github.com>
2019-07-30 03:39:03 +0300

    bug with cd

--- vkbot/vkbot/plugins/plugin_34.h
+++ vkbot/vkbot/plugins/plugin_34.h
@@ -4,7 +4,7 @@
 
 class Rule34Plugin : public Plugin {
 public:
-	int last_call;
+	std::map<long long, int> last_call;
 	Rule34Plugin(const std::vector<std::vector<std::string>>& command, CallArea call_area = CallArea::Any);
 	void exec(const std::vector<std::string>& args = {}, long peer_id = 0, long from_id = 0);
 	void update(std::string text, long peer_id = 0, long from_id = 0);
